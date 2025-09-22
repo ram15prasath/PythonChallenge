c8b699a504b4a10d169fddc5fe832161c448fffb
Ramprasath <ca235113646@bhc.edu.in>
2025-09-22 22:07:06 +0530

    Add files via upload

---
+++ Arithemetic_ope.py
@@ -0,0 +1,10 @@
+a=int(input("Enter number 1:"))
+b=int(input("Enter number 2:"))
+def arithemetic_operators():
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+    print(a%b)
+
+arithemetic_operators()
