58f9ddf3d66ec02078df779608bef3a2e9873594
Ramprasath <ca235113646@bhc.edu.in>
2025-09-23 21:40:16 +0530

    Add files via upload

---
+++ Comparision_ope.py
@@ -0,0 +1,11 @@
+a=int(input("Enter number 1:"))
+b=int(input("Enter number 2:"))
+def comparision_operators():
+    print(a==b)
+    print(a!=b)
+    print(a>b)
+    print(a<b)
+    print(a>=b)
+    print(a<=b)
+
+comparision_operators()    
