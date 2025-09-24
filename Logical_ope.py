fbf694d63c431958c98c03332b4152a2594ecb63
Ramprasath <ca235113646@bhc.edu.in>
2025-09-24 21:39:51 +0530

    Add files via upload

---
+++ Logical_ope.py
@@ -0,0 +1,8 @@
+a=int(input("Enter number 1:"))
+b=int(input("Enter number 2:"))
+def Logical_Operators():
+    print(a<b and b>a)
+    print(a<b or b>a)
+    print(not(a<b))
+
+Logical_Operators()    
