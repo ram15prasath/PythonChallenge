1dedbf9a7e80d830d47e5ef43ff29c64dce0c625
Ramprasath <ca235113646@bhc.edu.in>
2025-10-19 18:45:33 +0530

    Add files via upload

---
+++ Avg_Find.py
@@ -0,0 +1,8 @@
+def calculate_average(i, j):
+  """Calculates the average of two numbers, i and j."""
+  # The formula is (sum of numbers) / (count of numbers)
+  return (i + j) / 2
+
+# Test cases
+print(calculate_average(10, 20)) # Output: 15.0
+print(calculate_average(1, 2))   # Output: 1.5
