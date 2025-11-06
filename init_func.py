a19ae2a3e9d8c752712dfeb9bcb154e1bc1d39cf
Ramprasath <ca235113646@bhc.edu.in>
2025-11-06 19:04:02 +0530

    Add files via upload

---
+++ init_func.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+p1=Person("Ram",25) 
+p2=Person("Madhan",24)  
+p3=Person("Vijay",24) 
+print(p1.name)   
+print(p1.age)  
+print(p2.name)
+print(p2.age) 
+print(p3.name) 
+print(p3.age)   
