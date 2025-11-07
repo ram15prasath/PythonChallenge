05187b09a110314e11c48d8e3549f5658c32cddb
Ramprasath <ca235113646@bhc.edu.in>
2025-11-07 21:24:58 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+class Calculator:
+    def add(self,a,b):
+        return a+b
+    
+    def mul(self,a,b):
+        return a*b
+    
+calc=Calculator()
+print(calc.add(5,5))    
+print(calc.mul(5,5))
