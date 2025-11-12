65ed4345c670eda36355ce3e436454cbadb0a9cb
Ramprasath <ca235113646@bhc.edu.in>
2025-11-12 20:05:56 +0530

    Add files via upload

---
+++ num_guess_game01.py
@@ -0,0 +1,8 @@
+secret_num=25
+user_input=int(input("Enter the Guess"))
+
+if secret_num == user_input:
+    print("Your Guessing is right")
+else:
+    print("Your Guessing is wrong")
+               
